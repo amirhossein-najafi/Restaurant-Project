4b8cf163d1b67250d628f8b75211f2914838d6e6
amirhossein-najafi <njamir16@gmail.com>
2024-01-27 03:44:18 +0330

    responsive AdminPanel

--- src/pages/Addproducts.jsx
+++ src/pages/Addproducts.jsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react';
 import AdminNavbar from "../components/AdminNavbar.jsx";
-import img1 from "../assets/img/banner-1.jpg";
-import Button from "../layouts/Button.jsx";
 
 function Addproducts() {
     const options = [
@@ -25,21 +23,21 @@ function Addproducts() {
     }
     return (
         <div className="add">
-            <div className="bg-black min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 py-20">
+            <div className="bg-black min-h-screen flex flex-col  justify-center items-center sm:px-32 px-5 py-20">
                 <div className="fixed top-0 left-0">
                     <AdminNavbar/>
                 </div>
-                <div className="w-[45rem] h-[50rem] bg-[#8D684D] min-h-screen   rounded-[20px] pl-24 pt-12 pb-24 pr-24">
-                    <div className="flex flex-row justify-center  hover:text-brightColor font-extrabold">
+                <div className="lg:w-[45rem] lg:h-[50rem] md:w-[35rem] md:h-[40rem]  w-[20rem] h-[60rem] bg-[#8D684D] min-h-screen   rounded-[20px] pl-24 pt-12 pb-24 pr-24">
+                    <div className="flex flex-row justify-center sm:text-md  hover:text-brightColor font-extrabold">
                         ADD PRODUCT FORM
                     </div>
                     <div className="mt-12">
                         <span className="font-bold">Product Name</span><br/>
-                        <input className="text-center outline-none mt-2 w-96 h-10 rounded-[10px]" type="text" placeholder="Food Name"/>
+                        <input className="text-center outline-none mt-2 lg:w-96 md:w-60 w-52 h-10 rounded-[10px]" type="text" placeholder="Food Name"/>
                     </div>
-                    <div className="mt-12 flex flex-row justify-start gap-44">
+                    <div className="mt-12 flex flex-row justify-start lg:gap-44 md:gap-24 gap-14">
                         <div ><span className="font-bold">Category:</span><br/>
-                            <select  onChange={(e) => {handleChange(e)}} className="w-52 mt-2 h-10 rounded-[10px] pl-3">
+                            <select  onChange={(e) => {handleChange(e)}} className="lg:w-52 md:w-36 w-20 mt-2 h-10 rounded-[10px] pl-3">
                                 {options.map((option) => (
                                     <option value={option.value} className="" >{option.label}</option>
                                 ))}
@@ -47,26 +45,25 @@ function Addproducts() {
                         </div>
                         <div>
                             <span className="font-bold">price:</span>
-                            <input className="p-2 outline-none mt-2 w-52 h-10 rounded-[10px]" type="number" placeholder="Price"/>
+                            <input className="p-2 outline-none mt-2 lg:w-52 md:w-44 w-20 h-10 rounded-[10px]" type="number" placeholder="Price"/>
                         </div>
                     </div>
                     <div className="mt-12">
                         <span className="font-bold">Description:</span><br/>
-                        <input className="outline-none  w-10/12 h-28 rounded-[10px]" type="text" placeholder="Write details of food or anything else taht is important..."/>
+                        <input className="outline-none  lg:w-10/12 md:w-96 lg:h-28 md:h-16 h-16 w-52 rounded-[10px]" type="text" placeholder="Write details of food or anything else taht is important..."/>
                     </div>
 
-                    <div className="mt-12 flex flex-row justify-start gap-32">
+                    <div className="mt-12 flex flex-col md:flex-row justify-start md:gap-32">
                         <div>
                             <span className="font-bold">Image:</span><br/>
-                            <input className="bg-amber-50 outline-none   rounded-[10px]" type="file" multiple accept="image/*" onChange={handleChange1}/>
-
+                            <input className="bg-amber-50 outline-none md:w-36 lg:w-64 w-44     rounded-[10px]" type="file" multiple accept="image/*" onChange={handleChange1}/>
                         </div>
                         <div>
-                            <img className="mt-4" width="150px" height="150px"   src={file}  alt=""/>
+                            <img className="mt-4 "   src={file}  alt=""/>
 
                         </div>
                     </div>
-                    <div className="w-[35rem] flex justify-end gap-5 mt-12">
+                    <div className="lg:w-[37rem] md:w-[25rem] w-[19rem] flex justify-end gap-5 lg:mt-12 md:mt-36 mt-[4rem] ">
                         <button className="px-8 py-1 border-2  bg-brightColor border-brightColor text-black  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300  ">Save</button>
                         <button className="px-8 py-1 border-2  bg-brightColor border-brightColor text-black  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300  ">Save and Add</button>
 
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -13,17 +13,17 @@ function Edit() {
     ]
     return (
         <div>
-            <div className="bg-black min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 py-20">
+            <div className="bg-black h-screen flex flex-col justify-center items-center lg:px-32 px-5 py-20">
                 <div className="fixed top-0 left-0">
                     <AdminNavbar/>
                 </div>
-                <div className="mt-0 pt-0">
+                <div className="ml-5 mr-5  mt-0 pt-0">
                     <img src={img1} height="150" alt=""/>
                 </div>
                 <div className="mt-16">
-                    <table className="rounded-[20px] text-black bg-[#8D684D]  w-[60rem] h-[15rem]">
+                    <table className="rounded-[20px] text-black bg-[#8D684D]  lg:w-[60rem] lg:h-[15rem] md:w-[40rem] md:h-[15rem] sm:w-[10rem] sm:h-[2.5rem] w-[10rem] h-[2.5rem]">
                         <tr>
-                            <th className=" border-b-[1px] border-b-black">ProductID</th>
+                            <th className="pl-2 border-b-[1px] border-b-black">ProductID</th>
                             <th className="border-b-[1px] border-b-black">Name</th>
                             <th className="border-b-[1px] border-b-black">Details</th>
                             <th className="border-b-[1px] border-b-black">category</th>
@@ -32,21 +32,21 @@ function Edit() {
                         {data.map((val, key) => {
                             return (
                                 <tr key={key}>
-                                    <td className="pl-4 border-b-[1px] border-b-black">{val.ProductID}</td>
-                                    <td className="pl-4 border-b-[1px] border-b-black">{val.Name}</td>
-                                    <td className="pl-4 border-b-[1px] border-b-black">{val.Details}</td>
-                                    <td className="pl-4 border-b-[1px] border-b-black">{val.category}</td>
-                                    <td className="pl-4 border-b-[1px] border-b-black">
-                                        <input    type="checkbox" value=""
-                                                  className="border-footerColor accent-black  w-4 h-4 cursor-pointer rounded-[5px] "/>
+                                    <td className="sm:pl-2 md:pl-4 lg:pl-6 pl-2 border-b-[1px] border-b-black">{val.ProductID}</td>
+                                    <td className="sm:pl-2 md:pl-4 lg:pl-6 border-b-[1px] border-b-black">{val.Name}</td>
+                                    <td className="sm:pl-2 md:pl-4 lg:pl-6 border-b-[1px] border-b-black">{val.Details}</td>
+                                    <td className="sm:pl-2 md:pl-4 lg:pl-6 border-b-[1px] border-b-black">{val.category}</td>
+                                    <td className="sm:pl-2 md:pl-4 lg:pl-6 border-b-[1px] border-b-black">
+                                        <input    type="checkbox"
+                                                  className="border-footerColor accent-black pr-2  w-4 h-4 cursor-pointer rounded-[5px] "/>
                                     </td>
                                 </tr>
                             )
                         })}
                     </table>
                 </div>
-                <div className=" w-[55rem] flex justify-end gap-5 mt-4">
-                    <Button  title="edit"/>
+                <div className=" w-[55rem] flex justify-end  mt-4 sm:mr-[30rem] md:mr-72 lg:mr-0 gap-2 mr-[30rem] ">
+                    <Button title="edit"/>
                     <Button  title="delete"/>
                 </div>
 
